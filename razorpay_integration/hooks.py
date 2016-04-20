ba17a2d16372d086902e413cb2e33a43af271d15
Saurabh <saurabh6790@gmail.com>
2016-04-20 12:52:35 +0530

    [first commit]

---
+++ razorpay_integration/hooks.py
@@ -0,0 +1,113 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+app_name = "razorpay_integration"
+app_title = "Razorpay Integration"
+app_publisher = "Frappe Technologies Pvt. Ltd."
+app_description = "Razorpay Payment Gateway Integration"
+app_icon = "octicon octicon-file-directory"
+app_color = "grey"
+app_email = "info@frappe.io"
+app_version = "0.0.1"
+app_license = "MIT"
+
+# Includes in <head>
+# ------------------
+
+# include js, css files in header of desk.html
+# app_include_css = "/assets/razorpay_integration/css/razorpay_integration.css"
+# app_include_js = "/assets/razorpay_integration/js/razorpay_integration.js"
+
+# include js, css files in header of web template
+# web_include_css = "/assets/razorpay_integration/css/razorpay_integration.css"
+# web_include_js = "/assets/razorpay_integration/js/razorpay_integration.js"
+
+# Home Pages
+# ----------
+
+# application home page (will override Website Settings)
+# home_page = "login"
+
+# website user home page (by Role)
+# role_home_page = {
+#	"Role": "home_page"
+# }
+
+# Website user home page (by function)
+# get_website_user_home_page = "razorpay_integration.utils.get_home_page"
+
+# Generators
+# ----------
+
+# automatically create page for each record of this doctype
+# website_generators = ["Web Page"]
+
+# Installation
+# ------------
+
+# before_install = "razorpay_integration.install.before_install"
+# after_install = "razorpay_integration.install.after_install"
+
+# Desk Notifications
+# ------------------
+# See frappe.core.notifications.get_notification_config
+
+# notification_config = "razorpay_integration.notifications.get_notification_config"
+
+# Permissions
+# -----------
+# Permissions evaluated in scripted ways
+
+# permission_query_conditions = {
+# 	"Event": "frappe.desk.doctype.event.event.get_permission_query_conditions",
+# }
+#
+# has_permission = {
+# 	"Event": "frappe.desk.doctype.event.event.has_permission",
+# }
+
+# Document Events
+# ---------------
+# Hook on document methods and events
+
+# doc_events = {
+# 	"*": {
+# 		"on_update": "method",
+# 		"on_cancel": "method",
+# 		"on_trash": "method"
+#	}
+# }
+
+# Scheduled Tasks
+# ---------------
+
+# scheduler_events = {
+# 	"all": [
+# 		"razorpay_integration.tasks.all"
+# 	],
+# 	"daily": [
+# 		"razorpay_integration.tasks.daily"
+# 	],
+# 	"hourly": [
+# 		"razorpay_integration.tasks.hourly"
+# 	],
+# 	"weekly": [
+# 		"razorpay_integration.tasks.weekly"
+# 	]
+# 	"monthly": [
+# 		"razorpay_integration.tasks.monthly"
+# 	]
+# }
+
+# Testing
+# -------
+
+# before_tests = "razorpay_integration.install.before_tests"
+
+# Overriding Whitelisted Methods
+# ------------------------------
+#
+# override_whitelisted_methods = {
+# 	"frappe.desk.doctype.event.event.get_events": "razorpay_integration.event.get_events"
+# }
+
